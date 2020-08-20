6d6bcec01160653d0aa3c0ebc62319bc054db876
mritunjaymusale <mritunjaymusale@gmail.com>
2020-08-21 01:34:23 +0530

    added support for desktop site comment deletion

---
+++ main.py
@@ -0,0 +1,37 @@
+from python_imagesearch.imagesearch import imagesearch,click_image
+import time
+import pyautogui
+print("sleeping")
+time.sleep(5)
+
+def failsafeCheck():
+    current_mouse_position = pyautogui.position()
+    if current_mouse_position[0]< 250 and current_mouse_position[1]< 250 :
+        print("Fail Safe triggered object not found on screen")
+        if imagesearch('refresh_browser.png')!=[-1,-1]:
+            print('Refreshing browser')
+            click_image('refresh_browser.png',imagesearch('refresh_browser.png'),"left",0.0,offset=0)
+            print("sleeping till the page loads")
+            time.sleep(10)
+        current_mouse_position = pyautogui.position()
+
+        if current_mouse_position[0]< 250 and current_mouse_position[1]< 250 :
+            print("exiting")
+            exit()
+
+
+
+while True:
+    failsafeCheck()
+
+    if imagesearch('drop_down_menu.png')!= [-1,-1]:
+        click_image('drop_down_menu.png',imagesearch('drop_down_menu.png'),"left",0.0,offset=0)
+
+        if imagesearch('delete_option.png')!= [-1,-1]:    
+                click_image('delete_option.png',imagesearch('delete_option.png'),"left",0.0,offset=0)
+   
+    
+    else:
+        print("scrolling")
+        pyautogui.scroll(-10)
+
